d091cbf7a8a410be21704dfc594d89bfb35095bb
silvian.dragan <silvian.dragan@glintpay.com>
2018-03-05 01:03:51 +0000

    implemented books models

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,74 @@
-from django.contrib import admin
+"""Books admin."""
 
-# Register your models here.
+from django.contrib import admin
+from .models import (
+    Author,
+    Publisher,
+    Book,
+    ReadingList,
+)
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    """Author admin."""
+
+    list_display = ('name',)
+
+    search_fields = ('name',)
+
+
+@admin.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    """Publisher admin."""
+
+    list_display = ('name',)
+
+    search_fields = ('name',)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    """Book admin."""
+
+    list_display = (
+        'title',
+        'genre',
+        'publisher',
+        'published_date',
+        'pages',
+        'read',
+        'date_read',
+    )
+
+    search_fields = (
+        'title',
+        'genre',
+        'authors__name',
+        'publisher__name',
+        'published_date',
+    )
+
+    list_filter = (
+        'genre',
+        'read',
+    )
+
+
+@admin.register(ReadingList)
+class ReadingList(admin.ModelAdmin):
+    """ReadingList admin."""
+
+    list_display = (
+        'name',
+    )
+
+    search_fields = (
+        'books__title',
+        'books__genre',
+        'books__authors__name',
+        'books__publisher__name',
+        'books__publisher__published_date',
+    )
+
+    list_filter = ('books__read',)
--- books/models.py
+++ books/models.py
@@ -1,3 +1,139 @@
-from django.db import models
+"""Books models."""
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+from model_utils import Choices
+from django.utils.translation import ugettext_lazy as _
+
+
+class Author(models.Model):
+    """Author model."""
+
+    name = models.CharField(
+        max_length=200,
+    )
+    description = models.TextField(
+        blank=True,
+    )
+
+    def __str__(self):
+        """Return the string representation."""
+        return self.name
+
+
+class Publisher(models.Model):
+    """Publisher model."""
+
+    name = models.CharField(
+        max_length=200,
+    )
+    description = models.TextField(
+        blank=True,
+    )
+
+    def __str__(self):
+        """Return the string representation."""
+        return self.name
+
+
+class Book(models.Model):
+    """Books model."""
+
+    GENRES = Choices(
+        ('action', _('Action')),
+        ('adventure', _('Adventure')),
+        ('romance', _('Romance')),
+        ('fiction', _('Fiction')),
+        ('fantasy', _('Fantasy')),
+        ('non-fiction', _('Non Fiction')),
+        ('science-fiction', _('Science Fiction')),
+        ('satire', _('Satire')),
+        ('drama', _('Drama')),
+        ('mystery', _('Mystery')),
+        ('poetry', _('Poetry')),
+        ('comics', _('Comics')),
+        ('horror', _('Horror')),
+        ('art', _('Art')),
+        ('diaries', _('Diaries')),
+        ('guide', _('Guide')),
+        ('travel', _('Travel')),
+    )
+
+    title = models.CharField(
+        max_length=200,
+    )
+    genre = models.CharField(
+        max_length=20,
+        choices=GENRES,
+        default=GENRES.action,
+    )
+    description = models.TextField(
+        blank=True,
+    )
+    authors = models.ManyToManyField(
+        Author,
+        related_name="+",
+        verbose_name=_('author'),
+        blank=True,
+    )
+    publisher = models.ForeignKey(
+        Publisher,
+        related_name="+",
+        verbose_name=_('publisher'),
+        on_delete=models.CASCADE,
+        blank=True,
+    )
+    published_date = models.DateField(
+        blank=True,
+    )
+    pages = models.IntegerField(
+        blank=True,
+        null=True,
+    )
+    read = models.BooleanField(
+        default=False,
+    )
+    date_read = models.DateField(
+        blank=True,
+        null=True,
+    )
+
+    def mark_date_read(self):
+        """Mark date when book was read."""
+        self.date_read = timezone.now()
+
+    def clean_fields(self, exclude=None):
+        """Clean the fields needed for django admin."""
+        if self.read:
+            self.mark_date_read()
+        super().clean_fields(exclude=exclude)
+
+    def publish(self):
+        """Publish book."""
+        self.save()
+
+    def __str__(self):
+        """Return the string representation."""
+        return self.title
+
+
+class ReadingList(models.Model):
+    """Reading list model."""
+
+    name = models.CharField(
+        max_length=200,
+    )
+    books = models.ManyToManyField(
+        Book,
+        related_name="reading_list",
+        verbose_name=_('books'),
+        blank=True,
+    )
+
+    def publish(self):
+        """Publish reading list."""
+        self.save()
+
+    def __str__(self):
+        """Return the string representation."""
+        return self.name
