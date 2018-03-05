00f3423cd3229f15e68fcb7e7b8edce47c09d9aa
silvian.dragan <silvian.dragan@glintpay.com>
2018-03-05 01:03:51 +0000

    implemented books models

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,76 @@
-from django.contrib import admin
+"""Books admin."""
 
-# Register your models here.
+from django.contrib import admin
+from .models import (
+    Author,
+    Publisher,
+    Book,
+    ReadingList,
+)
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    """Author admin."""
+
+    list_display = ('name',)
+
+    search_fields = ('name',)
+
+
+@admin.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    """Publisher admin."""
+
+    list_display = ('name',)
+
+    search_fields = ('name',)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    """Book admin."""
+
+    list_display = (
+        'title',
+        'genre',
+        'pages',
+        'publisher',
+        'published_date',
+    )
+
+    search_fields = (
+        'title',
+        'genre',
+        'authors__name',
+        'publisher__name',
+        'published_date',
+    )
+
+    list_filter = (
+        'genre',
+    )
+
+
+@admin.register(ReadingList)
+class ReadingList(admin.ModelAdmin):
+    """ReadingList admin."""
+
+    list_display = (
+        'book',
+        'read',
+        'date_read',
+    )
+
+    search_fields = (
+        'book__title',
+        'book__genre',
+        'book__authors__name',
+        'book__publisher__name',
+        'book__published_date',
+    )
+
+    list_filter = (
+        'read',
+        'book__genre',
+    )
