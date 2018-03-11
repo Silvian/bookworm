97782edc21adf724a2165112b0180363f4c15827
Laurence Green <laurencegreen@tenpdlaptop024.home>
2018-03-11 13:47:46 +0000

    added basic serializers and viewsets

--- books/views.py
+++ books/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from books.models import Book
+from books.serializers import BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
