7633500674b296b047ab2c7c9cba02a1c19177f1
Laurence Green <laurencegreen@TENPDLAPTOP024.local>
2018-03-15 17:27:01 +0000

    moved meta information out into the expected model, added a selection of models to handle specifics related to reading and rating of a book

--- books/models.py
+++ books/models.py
@@ -14,6 +14,42 @@ from rest_framework.authtoken.models import Token
 from taggit.managers import TaggableManager
 
 
+# ------- start mixins
+
+
+class ModifiedModelMixin(models.Model):
+    """Modified field mixin.
+    """
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    modified_at = models.DateTimeField(
+        auto_now=True,
+        auto_now_add=False,
+    )
+
+
+class PreserveModelMixin(ModifiedModelMixin):
+    """Base model to handle core objects.
+
+    Defines created, modified, and deleted fields.
+    Prevents deletion of this model and flags for exclusion from results.
+    """
+
+    deleted_at = models.DateTimeField(
+        auto_now=False,
+        auto_now_add=False,
+        blank=True,
+    )
+
+
+# ------- end mixins
+
+
+# ------- start app: meta_info
+
+
 class Tag(models.Model):
     """Tag model.
 
@@ -29,8 +65,9 @@ class Tag(models.Model):
     )
     tags = models.ManyToManyField(
         'Tag',
-        related_name="+",
-        verbose_name=_('tags'),
+        related_name="tag_tags+",
+        verbose_name=_('Tags'),
+        on_delete=models.DO_NOTHING,
         blank=True,
     )
 
@@ -39,208 +76,147 @@ class Tag(models.Model):
         return self.name
 
 
-class Meta(models.Model):
+class Meta(ModifiedModelMixin):
     """Meta model."""
 
     key = models.CharField(
         max_length=200,
     )
     value = models.TextField()
-    created_at = models.DateTimeField(
-        auto_now_add=True,
-    )
-    modified_at = models.DateTimeField(
-        auto_now=True,
-        auto_now_add=False,
-    )
     tags = models.ManyToManyField(
         'Tag',
         related_name="meta_tags+",
-        verbose_name=_('tags'),
+        verbose_name=_('Tags'),
+        on_delete=models.DO_NOTHING,
         blank=True,
     )
 
 
-class Author(models.Model):
-    """Author model."""
-
-    name = models.CharField(
-        max_length=200,
-    )
-    description = models.TextField(
-        blank=True,
-    )
-
-    def __str__(self):
-        """Return the string representation."""
-        return self.name
+# ------- end app: meta_info
 
 
-class Publisher(models.Model):
-    """Publisher model."""
+# meta_information_example = {
+#     'genre': [],
+#     'author': '',
+#     'collaborators': [],
+#     'publisher': '',
+#     'distributor': '',
+#     'published_date': '',
+#     'publication_issue': '',
+#     'isbn': '',
+#     'barcode': '',
+#     'pages': '',
+#     'reviews': [],
+# }
+# GENRES = Choices(
+#     ('action', _('Action')),
+#     ('adventure', _('Adventure')),
+#     ('romance', _('Romance')),
+#     ('fiction', _('Fiction')),
+#     ('fantasy', _('Fantasy')),
+#     ('non-fiction', _('Non Fiction')),
+#     ('science-fiction', _('Science Fiction')),
+#     ('satire', _('Satire')),
+#     ('drama', _('Drama')),
+#     ('mystery', _('Mystery')),
+#     ('poetry', _('Poetry')),
+#     ('comics', _('Comics')),
+#     ('horror', _('Horror')),
+#     ('art', _('Art')),
+#     ('diaries', _('Diaries')),
+#     ('guide', _('Guide')),
+#     ('travel', _('Travel')),
+# )
 
-    name = models.CharField(
-        max_length=200,
-    )
-    description = models.TextField(
-        blank=True,
-    )
 
-    def __str__(self):
-        """Return the string representation."""
-        return self.name
-
-
-class Book(models.Model):
+class Book(PreserveModelMixin):
     """Books model."""
-
-    GENRES = Choices(
-        ('action', _('Action')),
-        ('adventure', _('Adventure')),
-        ('romance', _('Romance')),
-        ('fiction', _('Fiction')),
-        ('fantasy', _('Fantasy')),
-        ('non-fiction', _('Non Fiction')),
-        ('science-fiction', _('Science Fiction')),
-        ('satire', _('Satire')),
-        ('drama', _('Drama')),
-        ('mystery', _('Mystery')),
-        ('poetry', _('Poetry')),
-        ('comics', _('Comics')),
-        ('horror', _('Horror')),
-        ('art', _('Art')),
-        ('diaries', _('Diaries')),
-        ('guide', _('Guide')),
-        ('travel', _('Travel')),
-    )
 
     title = models.CharField(
         max_length=200,
     )
-    genre = models.CharField(
-        max_length=20,
-        choices=GENRES,
-        default=GENRES.action,
-    )
     description = models.TextField(
         blank=True,
-    )
-    pages = models.PositiveIntegerField(
-        blank=True,
-        null=True,
-    )
-    authors = models.ManyToManyField(
-        Author,
-        related_name="+",
-        verbose_name=_('author'),
-        blank=True,
-    )
-    publisher = models.ForeignKey(
-        Publisher,
-        related_name="+",
-        verbose_name=_('publisher'),
-        on_delete=models.PROTECT,
-        blank=True,
-    )
-    published_date = models.DateField(
-        blank=True,
-        null=True,
     )
     tags = models.ManyToManyField(
         'Tag',
         related_name="book_tags+",
-        verbose_name=_('tags'),
+        verbose_name=_('Tags'),
+        on_delete=models.DO_NOTHING,
         blank=True,
     )
-
-    def publish(self):
-        """Publish book."""
-        self.save()
+    meta = models.ForeignKey(
+        'Meta',
+        related_name="book_meta+",
+        verbose_name=_('Meta Information'),
+        blank=True,
+    )
 
     def __str__(self):
         """Return the string representation."""
         return self.title
 
 
-class Favourite(models.Model):
+class BookProgress(Meta, PreserveModelMixin):
+    """Favourite books model."""
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    percent = models.FloatField(
+        blank=True,
+    )
+    page = models.PositiveSmallIntegerField(
+        blank=True,
+    )
+    progress = models.BigIntegerField(
+        blank=True,
+    )
+    book = models.ForeignKey(
+        'Book',
+        related_name="book_progress+",
+        verbose_name=_('Book'),
+        on_delete=models.DO_NOTHING,
+        blank=True,
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.DO_NOTHING,
+    )
+
+    def __str__(self):
+        """Return the string representation."""
+        return self.book.title
+
+
+class BookReview(Meta, PreserveModelMixin):
     """Favourite books model."""
 
     book = models.ForeignKey(
-        Book,
-        related_name="+",
-        verbose_name=_('book'),
+        'Book',
+        related_name="book_reviews",
+        verbose_name=_('Book'),
         on_delete=models.PROTECT,
+        blank=True,
+    )
+    progress = models.ForeignKey(
+        'BookProgress',
+        related_name="book_review_progress",
+        verbose_name=_('Book Progress'),
+        on_delete=models.DO_NOTHING,
         blank=True,
     )
     user = models.ForeignKey(
         User,
-        on_delete=models.PROTECT,
+        on_delete=models.DO_NOTHING,
     )
-
-    class Meta:
-        unique_together = ('book', 'user')
 
     def __str__(self):
         """Return the string representation."""
         return self.book.title
 
 
-class ReadingList(models.Model):
-    """Reading list model."""
-
-    book = models.ForeignKey(
-        Book,
-        related_name="+",
-        verbose_name=_('book'),
-        on_delete=models.PROTECT,
-        blank=True,
-    )
-    started_reading = models.BooleanField(
-        default=False,
-    )
-    started_date = models.DateField(
-        blank=True,
-        null=True,
-    )
-    finished_reading = models.BooleanField(
-        default=False,
-    )
-    finished_date = models.DateField(
-        blank=True,
-        null=True,
-    )
-    user = models.ForeignKey(
-        User,
-        on_delete=models.PROTECT,
-    )
-
-    def mark_date_started(self):
-        """Mark date when book was started."""
-        self.started_date = now().date()
-
-    def mark_date_read(self):
-        """Mark date when book was read."""
-        self.finished_date = now().date()
-
-    def save(self, *args, **kwargs):
-        """Override to perform validations."""
-        if self.started_reading:
-            self.mark_date_started()
-        if self.finished_reading:
-            self.mark_date_read()
-            if not self.started_reading:
-                self.started_reading = True
-            if not self.started_date:
-                self.mark_date_started()
-        super().save(*args, **kwargs)
-
-    def publish(self):
-        """Publish reading list."""
-        self.save()
-
-    def __str__(self):
-        """Return the string representation."""
-        return self.book.title
+# ------- start app: user_profile
 
 
 class Profile(models.Model):
@@ -283,3 +259,6 @@ def create_auth_token(sender, instance=None, created=False, **kwargs):
     """Generate authentication API token for a created user instance."""
     if created:
         Token.objects.create(user=instance)
+
+
+# ------- end app: user_profile
